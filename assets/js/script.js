ea77393503b423a1a1caa2cb4244a72507f4a627
emilyk <emily.g.kruse@gmail.com>
2022-03-16 15:26:18 -0400

    save search history to localStorage, change uvi background color based on value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,86 @@
 let searchFormEl = document.querySelector("#search-form");
-let searchHistoryEl = document.querySelector(".search-history");
+let searchHistoryListEl = document.querySelector("#search-history-list");
 let cityInputEl = document.querySelector("#city");
+let currWeatherContEl = document.querySelector(".current-weather");
 let currWeatherListEl = document.querySelector("#current-weather-list");
+let searchedCities = [];
 
-// let formSubmitHandler = function(event) {
-//   event.preventDefault();
-//   // get value from input element
-//   let city = cityInputEl.value.trim();
+// hide border around current weather container when page is initially loaded
+currWeatherContEl.style.display = "none";
 
-//   // if value is valid, send to getCoordinates and clear form input
-//   if (city) {
-//     getCoordinates(city);
-//     cityInputEl.value = "";
-//   }
-//   // if value is not valid, send alert
-//   else {
-//     alert("Please enter a name of a city");
-//   }
-// };
+// hide forecast cards when page is loaded
+for (let i = 0; i < 5; i++) {
+  let forecastCardEl = document.querySelector("#day" + (i+1));
+  forecastCardEl.style.display = "none";
+}
+
+// load list of cities previously searched from localStorage
+let loadSearchHistory = function() {
+  // get stored cities
+  searchedCities = JSON.parse(localStorage.getItem("cities"));
+
+  // if no stored cities, create empty array
+  if (!searchedCities) {
+    searchedCities = [];
+  }
+  // create a list of city buttons and append to search history list
+  else {
+    for (let i = 0; i < searchedCities.length; i++) {
+      let cityListEl = document.createElement("li");
+      cityListEl.id = searchedCities[i];
+      cityListEl.classList = "searched-city btn";
+      cityListEl.textContent = searchedCities[i];
+  
+      searchHistoryListEl.appendChild(cityListEl);
+    }
+  }
+}
+
+let formSubmitHandler = function(event) {
+  event.preventDefault();
+
+  // get value from input element
+  let city = cityInputEl.value.trim();
+  // if value is valid, send to getCoordinates, clear form input, and send city to local storage if not already there
+  if (city) {
+    getCoordinates(city);
+    cityInputEl.value = "";
+    searchedCities = JSON.parse(localStorage.getItem("cities"));
+    if (!searchedCities) {
+      searchedCities = [];
+      searchedCities.push(city);
+      saveSearchedCities();
+    }
+    else {
+      let index = searchedCities.findIndex(function(searchedCity) {
+        return JSON.stringify(searchedCity) === JSON.stringify(city);
+      })
+      if (index === -1) {
+        searchedCities.push(city);
+        saveSearchedCities();
+      }
+    }
+  }
+  // if value is not valid, send alert
+  else {
+    alert("Please enter a name of a city");
+  } 
+};
+
+// when city button in search history is clicked, get the city name and send to getCoordinates
+let cityButtonHandler = function(event) {
+  event.preventDefault();
+  let targetEl = event.target;
+  let city = targetEl.id.value;
+
+  if (city) {
+    getCoordinates(city);
+  }
+}
 
 let getCoordinates = function(city) {
   // format open weather geocoding api url
-  let apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=${weatherApiKey}";
+  let apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=a4a152b1648968c8a4d183d5f5b5283f";
 
   fetch(apiUrl)
     .then(function(response) {
@@ -45,7 +104,7 @@ let getCoordinates = function(city) {
 
 let getWeather = function(lat, long, city) {
   // format the open weather api url
-  let apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&exclude=minutely,hourly&appid=${weatherApiKey}";
+  let apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&units=imperial&exclude=minutely,hourly&appid=a4a152b1648968c8a4d183d5f5b5283f";
   
   // make a request to the url
   fetch(apiUrl)
@@ -53,7 +112,6 @@ let getWeather = function(lat, long, city) {
       // request was successful
       if (response.ok) {
         response.json().then(function(data) {
-          console.log(data);
           displayWeather(data, city);
         });
       }
@@ -85,11 +143,18 @@ let displayWeather = function(data, city) {
   // create list for current weather conditions
   for (let i = 0; i < 4; i++) {
     let conditions = ["Temp", "Wind", "Humidity", "UV Index"];
-    let condEl = document.createElement("li");
-    condEl.id = conditions[i];
-    condEl.textContent = conditions[i] + ": ";
+    let condEl = document.getElementById(conditions[i]);
 
-    currWeatherListEl.appendChild(condEl);
+    if (!condEl) {
+      condEl = document.createElement("li");
+      condEl.id = conditions[i];
+      condEl.textContent = conditions[i] + ": ";
+
+      currWeatherListEl.appendChild(condEl);
+    }
+    else {
+      condEl.textContent = conditions[i] + ": ";
+    }
   }
 
   // add current temp to current conditions
@@ -106,18 +171,36 @@ let displayWeather = function(data, city) {
 
   // add current uv index to current conditions
   let uvi = document.getElementById("UV Index");
-  uvi.textContent += data.current.uvi;
+  // add appropriate class for green background when uvi is in favorable range
+  if (data.current.uvi < 3.00) {
+    uvi.innerHTML += "<span class='UV-green'>" + data.current.uvi + "</span>";
+  }
+  // add appropriate class for yellow background when uvi is in moderate range
+  else if (data.current.uvi >= 3.00 && data.current.uvi < 7.99) {
+    uvi.innerHTML += "<span class='UV-yellow'>" + data.current.uvi + "</span>";
+  }
+  // add appropriate class for red background when uvi is in severe range
+  else if (data.current.uvi >= 8.00) {
+    uvi.innerHTML += "<span class='UV-red'>" + data.current.uvi + "</span>";
+  }
+  
+  currWeatherContEl.style.display = "block";
   
   // create a title for forecast container
   let forecastTitleContEl = document.querySelector(".forecast-title-container")
-  let forecastTitleEl = document.createElement("h3");
-  forecastTitleEl.classList = "forecast-title";
-  forecastTitleEl.textContent = "5-day forecast:";
-  forecastTitleContEl.appendChild(forecastTitleEl);
+  let forecastTitleEl = document.querySelector(".forecast-title");
+
+  if (!forecastTitleEl) {
+    let forecastTitleEl = document.createElement("h3");
+    forecastTitleEl.classList = "forecast-title";
+    forecastTitleEl.textContent = "5-day forecast:";
+    forecastTitleContEl.appendChild(forecastTitleEl);
+  }
 
   // add date and weather icon and data to each of the forecast cards
   for (let i = 0; i < 5; i++) {
     let forecastCardEl = document.querySelector("#day" + (i+1));
+    forecastCardEl.style.display = "flex";
     let forecastDate = luxon.DateTime.now().plus({ days: (i+1) }).toLocaleString();
     forecastCardEl.textContent = forecastDate;
 
@@ -146,19 +229,28 @@ let displayWeather = function(data, city) {
     }
 
     // add forecasted temp to forecast cards
-  let fTemp = document.getElementById("Temp" + i);
-  fTemp.textContent += data.daily[i].temp.day + "°F";
+    let fTemp = document.getElementById("Temp" + i);
+    fTemp.textContent += data.daily[i].temp.day + "°F";
 
-  // add forecasted wind speed to forecast cards
-  let fWind = document.getElementById("Wind" + i);
-  fWind.textContent += data.daily[i].wind_speed + " MPH";
+    // add forecasted wind speed to forecast cards
+    let fWind = document.getElementById("Wind" + i);
+    fWind.textContent += data.daily[i].wind_speed + " MPH";
 
-  // add forecasted humidity to forecast cards
-  let fHumid = document.getElementById("Humidity" + i);
-  fHumid.textContent += data.daily[i].humidity + " %";
+    // add forecasted humidity to forecast cards
+    let fHumid = document.getElementById("Humidity" + i);
+    fHumid.textContent += data.daily[i].humidity + " %";
   }
 }
 
-//searchFormEl = addEventListener("submit", formSubmitHandler);
+let saveSearchedCities = function() {
+  // save cities searched to localStorage
+  localStorage.setItem("cities", JSON.stringify(searchedCities));
 
-getCoordinates("Columbus");
+  // add city to search history display
+  loadSearchHistory();
+}
+
+searchFormEl = addEventListener("submit", formSubmitHandler);
+//searchHistoryListEl = addEventListener("click", cityButtonHandler);
+
+loadSearchHistory();
